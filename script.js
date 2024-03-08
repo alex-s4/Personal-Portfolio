83018bf06e0c777e1c81b819d20559f35cd75d5d
alex-s4 <al39726867@gmail.com>
2024-03-07 22:15:27 -0500

    Add dropdown menu for projects tab

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// NUMBERS FROM LANDING PAGE WILL COUNT
+
 const counters = document.querySelectorAll('.value');
 const speed = 10;
 
@@ -8,7 +10,7 @@ counters.forEach( counter => {
      
       const time = value / speed;
      if(data < value) {
-          counter.innerText = Math.ceil(data + time) + "+";
+          counter.innerText = Math.ceil(data + time);
           setTimeout(animate, 100);
         }else{
           counter.innerText = value + "+";
@@ -18,3 +20,34 @@ counters.forEach( counter => {
    
    animate();
 })
+
+
+// NAV DROPDOWN WILL APPEAR UPON HOVER
+
+const navProj = document.querySelector("#nav-feat-proj");
+const navProjText = document.querySelector("#nav-feat-proj a");
+const navProjDropDown = document.querySelector("#feat-proj-dropdown");
+
+navProj.onmouseover = function(){
+    console.log("ugly proj")
+    navProjDropDown.style.display = "block";
+    navProjText.style.borderBottom = "5px solid #fff";
+}
+
+navProjDropDown.onmouseover = function(){
+    console.log("ugly proj")
+    navProjDropDown.style.display = "block";
+    navProjText.style.borderBottom = "5px solid #fff";
+}
+
+navProj.onmouseout = function(){
+    console.log("ugly proj")
+    navProjDropDown.style.display = "none";
+    navProjText.style.borderBottom = "none";
+}
+
+navProjDropDown.onmouseout = function(){
+    console.log("ugly proj")
+    navProjDropDown.style.display = "none";
+    navProjText.style.borderBottom = "none";
+}
