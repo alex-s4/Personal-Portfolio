45990e81f7e68876826101d6d39f87f2fbb3d141
alex-s4 <al39726867@gmail.com>
2024-03-09 05:32:07 -0500

    add contact dropdown

--- script.js
+++ script.js
@@ -36,27 +36,47 @@ counters.forEach( counter => {
 const navProj = document.querySelector("#nav-feat-proj");
 const navProjText = document.querySelector("#nav-feat-proj a");
 const navProjDropDown = document.querySelector("#feat-proj-dropdown");
+const navConnect = document.querySelector("#nav-contact");
+const navConnectText = document.querySelector("#nav-contact a");
+const navConnectDropDown = document.querySelector("#connect-dropdown");
+
 
 navProj.onmouseover = function(){
-    console.log("ugly proj")
     navProjDropDown.style.display = "block";
     navProjText.style.borderBottom = "5px solid #fff";
 }
 
 navProjDropDown.onmouseover = function(){
-    console.log("ugly proj")
     navProjDropDown.style.display = "block";
     navProjText.style.borderBottom = "5px solid #fff";
 }
 
 navProj.onmouseout = function(){
-    console.log("ugly proj")
     navProjDropDown.style.display = "none";
     navProjText.style.borderBottom = "none";
 }
 
 navProjDropDown.onmouseout = function(){
-    console.log("ugly proj")
     navProjDropDown.style.display = "none";
     navProjText.style.borderBottom = "none";
+}
+
+navConnect.onmouseover = function(){
+  navConnectDropDown.style.display = "block";
+  navConnectText.style.borderBottom = "5px solid #fff";
+}
+
+navConnectDropDown.onmouseover = function(){
+  navConnectDropDown.style.display = "block";
+  navConnectText.style.borderBottom = "5px solid #fff";
+}
+
+navConnect.onmouseout = function(){
+  navConnectDropDown.style.display = "none";
+  navConnectText.style.borderBottom = "none";
+}
+
+navConnectDropDown.onmouseout = function(){
+  navConnectDropDown.style.display = "none";
+  navConnectText.style.borderBottom = "none";
 }
